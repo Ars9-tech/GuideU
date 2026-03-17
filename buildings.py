b2323c7a8e871182d8d8cb56b29964f86735bed6
Ars9-tech <aradhanasoni214@gmail.com>
2026-03-17 18:42:57 +0530

    Refactor building data structure and add locations
    Updated building information and added new locations.

--- buildings.py
+++ buildings.py
@@ -1,48 +1,28 @@
-buildings = {
+locations = {
 
-"A Block":{
-"category":"Academic",
-"description":"Academic block with lecture rooms"
+"A Block": {
+"category": "Academic",
+"description": "Academic block with lecture rooms and hatchery."
 },
 
-"B Block":{
-"category":"Academic",
-"description":"Labs and classrooms"
+"Gobble": {
+"category": "Mess",
+"description": "Gobble mess located near D1 hostel."
 },
 
-"Library":{
-"category":"Academic",
-"description":"Central library for students"
+"Girls Hostel D1": {
+"category": "Hostel",
+"description": "Girls hostel building D1."
 },
 
-"Girls Hostel D1":{
-"category":"Hostel",
-"description":"Girls hostel building D1"
+"Boys Hostel C1": {
+"category": "Hostel",
+"description": "Boys hostel building C1."
 },
 
-"Boys Hostel C1":{
-"category":"Hostel",
-"description":"Boys hostel building C1"
-},
-
-"Subway":{
-"category":"Food",
-"description":"Subway outlet in N block"
-},
-
-"Dominos":{
-"category":"Food",
-"description":"Dominos outlet in N block ground floor"
-},
-
-"Hotspot":{
-"category":"Food",
-"description":"Food outlet near C1 hostel"
-},
-
-"Tuckshop":{
-"category":"Food",
-"description":"Tuckshop located under C7"
+"Food Outlets": {
+"category": "Food",
+"description": "Food outlets available on campus including Subway and Dominos in N Block."
 }
 
 }
