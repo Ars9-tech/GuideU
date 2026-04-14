f11d1645f85ab684ea09cd5fe12057cff2675850
Ars9-tech <aradhanasoni214@gmail.com>
2026-04-14 14:26:58 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
+# database.py
 import sqlite3
+import hashlib
+
+def hash_pw(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
-
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT UNIQUE,
-password TEXT
-)
+    CREATE TABLE IF NOT EXISTS users (
+        id       INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
 """)
-
 conn.commit()
 conn.close()
